28a59fd6e37ebd11e6f2fd31a9b51580a4001537
Amiy <amiy.srivastava@fujitsu.com>
2021-10-19 16:07:46 +0530

    added git remote command

--- myApp.js
+++ myApp.js
@@ -1,1 +1,8 @@
-This is version 2 app
+This is version 3 app
+echo "# deveOps" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AmiySrivastava/deveOps.git
+git push -u origin main
